2a743e03d40d667ec6fec3bff42d1e628c4311bf
ilhammajidzaiman <ilhammajidzaiman07@gmail.com>
2024-05-03 11:00:10 +0700

    fix: template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,18 +7,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ env('APP_NAME') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('image/laravel.svg') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/bootstrap/icons/font/bootstrap-icons.css') }}">
+
+    <style>
+        body {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
 </head>
 
-<body>
-
-    {{-- navbar section --}}
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm bg-white text-dark bg-opacity-75 fw-medium"
+<body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm bg-white text-dark bg-opacity-75"
         aria-label="Offcanvas navbar large">
-        <div class="container py-2">
-            <a class="navbar-brand fw-bold fs-3 text-success-emphasis" href="/">
+        <div class="container py-1">
+            <a class="navbar-brand text-success-emphasis" href="/">
                 <img src="{{ asset('image/laravel.svg') }}" alt="Logo" width="35" height="35"
                     class="d-inline-block align-text-top">
                 <span class="d-inline d-lg-none">
@@ -42,7 +49,7 @@
                 <div class="offcanvas-body">
                     <ul class="navbar-nav flex-grow-1 me-auto text-capitalize">
                         <li class="nav-item">
-                            <a href="/dashboard" aria-current="page" class="nav-link active">
+                            <a href="/dashboard" aria-current="page" class="nav-link">
                                 Dashboard
                             </a>
                         </li>
@@ -84,9 +91,27 @@
 
     {{-- container section --}}
     <section class="container mt-5 py-5">
-        <h1>Dashboard</h1>
+        <header class="my-3">
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="/">Dashboard</a>
+                    </li>
+                </ol>
+            </nav>
+            <h1>Dashboard</h1>
+        </header>
+        <main>
+            <div class="card border-0 shadow-sm">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                        of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </main>
     </section>
-
 
     <script src="{{ asset('/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
